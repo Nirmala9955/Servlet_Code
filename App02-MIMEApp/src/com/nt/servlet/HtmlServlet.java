0e4040808f47c9d486f884952d4482458e5b89fa
Nirmala kumar sahu <nirmalakumarsahu7@gmail.com>
2020-07-16 07:48:09 +0530

    App02-MIMEApp
    using different MIME type for displaying the different outputs like xml,
    html, word, excel

---
+++ App02-MIMEApp/src/com/nt/servlet/HtmlServlet.java
@@ -0,0 +1,30 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HtmlServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		PrintWriter pw = null;
+		//get PrintWriter object
+		pw=res.getWriter();
+		//set contentType
+		res.setContentType("text/html");
+		//write logic to generate output (web page)
+		pw.println("<table border='1'>");
+		pw.println("<tr><th>Player</th><th>Role</th></tr>");
+		pw.println("<tr><td>Dhoni</td><td>Captain</td></tr>");
+		pw.println("<tr><td>Sachin</td><td>All rounder</td></tr>");
+		pw.println("</table>");
+		//close stream
+		pw.close();
+	} //service(-,-)
+
+} //class
